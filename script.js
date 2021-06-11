2c9d0385eb5f8474dfe85370d533c3a9da71a1c8
Megan Buck <thequiltingriverotter@gmail.com>
2021-06-10 16:09:40 -1000

    Added list of chinese terms for more varied testing, took out console.log testing code, took out html paragraph placeholder text.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-chineseTerms = [['zhongwen', 'zhong1wen2', 'Chinese'],
-['nihao', 'ni2hao3', 'hello'],
-['long', 'long2', 'dragon'],
-['shafa', 'sha1fa1', 'sofa'],];
+chineseTerms = [
+    ["挨", "ái", "Verb", "to endure, to suffer"],
+    ["爱不释手", "ài bù shì shǒu", "Verb", "not wanting to give sth. out of one's hand"],
+    ["爱戴", "ài dài", "Verb", "to love and respect"],
+    ["暧昧", "ài mèi", "Adjective", "ambiguous, equivocal, dubious"],
+    ["哎哟", "āi yō", "Interjunction", "ow, ouch, ah"],
+    ["癌症", "ái zhèng", "Noun", "cancer"],
+    ["案件", "àn jiàn", "Noun", "legal case, case"],
+    ["安居乐业", "ān jū lè yè", "Verb", "to live in peace and work happily"],
+    ["案例", "àn lì", "Noun", "case (law)"],
+    ["按摩", "àn mó", "Noun/Verb", "massage, to massage"],
+    ["安宁", "ān níng", "Noun/Adjective", "peace, tranquil"],
+    ["暗示", "àn shì", "Noun/Verb", "hint, suggestion"],
+    ["安详", "ān xiáng", "Adjective", "composed, serene"],
+    ["安置", "ān zhì", "Verb", "to find a place for, to arrange for"],
+    ["昂贵", "áng guì", "Adjective", "expensive, costly"],
+    ["熬", "áo", "Verb", "to endure, to suffer"],
+    ["奥秘", "ào mì", "Noun", "mystery, secret"],
+    ["凹凸", "āo tū", "Adjective", "bumpy, uneven"]
+];
 
 
 const cardText = document.getElementById("card");
@@ -16,7 +32,6 @@ let term = [];
 function generateTerm() {
     let randomNumber = Math.floor(Math.random() * chineseTerms.length);
     term = chineseTerms[randomNumber];
-    console.log(term);
 }
 generateTerm();
 
@@ -27,7 +42,7 @@ function showPinyin() {
     cardText.innerHTML = term[1];
 }
 function showEnglish() {
-    cardText.innerHTML = term[2];
+    cardText.innerHTML = term[3];
 }
 
 newBtn.addEventListener("click", generateTerm);
